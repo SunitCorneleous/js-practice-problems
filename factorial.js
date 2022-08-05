54f6d31b849708866b957eb33a4fcb30a23a4a06
Sunit <corneleous25@gmail.com>
2022-08-05 17:25:39 +0600

    factorial using while loop

--- factorial.js
+++ factorial.js
@@ -13,4 +13,28 @@ function getFactorial(number) {
 const num = 6;
 
 const result = getFactorial(num);
-console.log("The factorial of " + num + " is " + result);
+console.log("------------------------------------");
+console.log("| Using for loop,");
+console.log("| The factorial of " + num + " is: " + result);
+
+// write a functin to find factorial of any number using while loop
+
+function getFactorialWhile(number) {
+  let count = number;
+  let result = 1;
+
+  while (count > 1) {
+    result *= count;
+    count--;
+  }
+
+  return result;
+}
+
+const num2 = 8;
+const output = getFactorialWhile(num2);
+
+console.log("------------------------------------");
+console.log("| Using while loop,");
+console.log("| The factorial of " + num2 + " is: " + output);
+console.log("------------------------------------");
