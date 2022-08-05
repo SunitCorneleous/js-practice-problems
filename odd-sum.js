25c4f3fd654522e55129018e44db40257c4266d0
Sunit Corneleous <sunit15-10591@diu.edu.bd>
2022-08-05 14:51:10 +0600

    find all odd numbers problem added

--- odd-sum.js
+++ odd-sum.js
@@ -10,7 +10,24 @@ function sumOfArray(array) {
   return sum;
 }
 
+// write a functin to get all odd numbers in an aray
+
+function getOddNumbers(array) {
+  let oddArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
+      oddArray.push(array[i]);
+    }
+  }
+  return oddArray;
+}
+
 const numbersArray = [25, 10, 16, 19, 17, 34, 60, 70, 33, 15, 14, 87];
 let result = sumOfArray(numbersArray);
+let oddNumbers = getOddNumbers(numbersArray);
 
-console.log("The sum of odd numbers are", result);
+console.log("The numbers are:", numbersArray);
+console.log("The odd numbers are:", oddNumbers);
+console.log("-------------------------------------");
+console.log("The sum of odd numbers are:", result);
+console.log("-------------------------------------");
