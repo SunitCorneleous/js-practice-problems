935b4d3a66042710137a7566a09a375ff050ddaa
Sunit Corneleous <sunit15-10591@diu.edu.bd>
2022-08-05 12:27:01 +0600

    find even number problem added

--- even-odd.js
+++ even-odd.js
@@ -1,1 +1,17 @@
 // Write a function to find even numbers
+
+function findEvenNum(num) {
+  let result = "";
+  if (num % 2 === 0) {
+    result = "The number is even";
+  } else {
+    result = "The number is odd";
+  }
+
+  return result;
+}
+
+const number = 15;
+
+const isEven = findEvenNum(number);
+console.log(isEven);
