8e94ddf7934e325ea66214b6ee40e2e690bfa874
Sunit <corneleous25@gmail.com>
2022-08-06 17:16:34 +0600

    added switch in find-grade

--- find-grade.js
+++ find-grade.js
@@ -1,4 +1,4 @@
-// write a function to find your grade
+// write a function to find your grade using if-else
 
 function getGrade(marks) {
   if (marks >= 80) {
@@ -28,3 +28,42 @@ const result = getGrade(45);
 console.log("----------------------------------------");
 console.log("         Your grade is", result);
 console.log("----------------------------------------");
+
+// write a function to find your grade using switch
+function getGradeSwitch(marks) {
+  switch (true) {
+    case marks >= 80:
+      console.log("A+");
+      break;
+    case marks >= 75 && marks < 80:
+      console.log("A");
+      break;
+    case marks >= 70 && marks < 75:
+      console.log("A-");
+      break;
+    case marks >= 65 && marks < 70:
+      console.log("B+");
+      break;
+    case marks >= 60 && marks < 65:
+      console.log("B");
+      break;
+    case marks >= 55 && marks < 60:
+      console.log("B-");
+      break;
+    case marks >= 50 && marks < 55:
+      console.log("C");
+      break;
+    case marks >= 45 && marks < 50:
+      console.log("C-");
+      break;
+    case marks >= 40 && marks < 45:
+      console.log("D");
+      break;
+
+    default:
+      console.log("Fail");
+      break;
+  }
+}
+
+getGradeSwitch(55);
